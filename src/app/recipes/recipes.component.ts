86d61003589520bfbf6f3cbf7f5667f9372d397c
sparsh97 <sparshvermadmc@gmail.com>
2021-08-12 23:11:39 +0530

    started working on recipe component

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.scss']
+})
+export class RecipesComponent implements OnInit {
+  recipes: Recipe[]=[
+    new Recipe("Chicken","chicken curry","https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F5064952.jpg&q=85")
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
