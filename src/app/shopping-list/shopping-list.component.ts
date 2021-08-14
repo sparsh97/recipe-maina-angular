391dd83b07c4db0d182523e87a68c6b377de1b8b
sparsh97 <sparshvermadmc@gmail.com>
2021-08-14 19:05:05 +0530

    recipe list and shopping list changes

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients:Ingredient[] = [
+    new Ingredient("Tomato",4),
+    new Ingredient("Apple",4),
+   ];
   constructor() { }
 
   ngOnInit(): void {
